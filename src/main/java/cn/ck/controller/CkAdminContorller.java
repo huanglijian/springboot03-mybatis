2b1028ab9587ec6487a6eb9ffc032c0648bdab61
AchmGitHub <670509577@qq.com>
2018-09-20 12:47:28 +0800

    添加注释 陈文翰

--- src/main/java/cn/ck/controller/CkAdminContorller.java
+++ src/main/java/cn/ck/controller/CkAdminContorller.java
@@ -3,6 +3,10 @@ package cn.ck.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 此控制器
+ * 用于测试页面
+ */
 @Controller
 @RequestMapping("/ckadmin")
 public class CkAdminContorller {
