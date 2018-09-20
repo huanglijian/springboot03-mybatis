a2c6c878d5144552e65fa484f531451196cb96e2
AchmGitHub <670509577@qq.com>
2018-09-20 23:28:17 +0800

    登录功能，解决验证码，放弃记住我 陈文翰

--- src/main/java/cn/ck/exception/GlobalExceptionHandler.java
+++ src/main/java/cn/ck/exception/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package cn.ck.exception;
 import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 @ControllerAdvice
@@ -13,6 +16,16 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(value = AuthorizationException.class)
 	public String handleAuthorizationException() {
-		return "error/403";
+		return "error/500";
+	}
+
+	@ExceptionHandler(DuplicateKeyException.class)
+	public String handleDuplicateKeyException(DuplicateKeyException e){
+		return "error/500";
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String handleException(Exception e){
+		return "error/500";
 	}
 }
