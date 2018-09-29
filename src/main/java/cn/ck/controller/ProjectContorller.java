535bcba8023b182679d3250d593aa7269c6e9e3c
SapphireXia <782788572@qq.com>
2018-09-29 12:25:03 +0800

    1

--- src/main/java/cn/ck/controller/ProjectContorller.java
+++ src/main/java/cn/ck/controller/ProjectContorller.java
@@ -1,6 +1,9 @@
 package cn.ck.controller;
 
 import cn.ck.entity.Bidding;
+
+import java.util.*;
+
 import cn.ck.entity.Project;
 import cn.ck.entity.Promulgator;
 import cn.ck.entity.Studio;
@@ -9,15 +12,14 @@ import cn.ck.service.ProjectService;
 import cn.ck.service.PromulgatorService;
 import cn.ck.service.StudioService;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -39,10 +41,10 @@ public class ProjectContorller {
     public String xia1(){
         //shfit+F6 == 重命名
        //查询所有项目的记录
-        List<Project> projects = projectService.selectList(new EntityWrapper<>());
-        for(Project attribute : projects) {
-            System.out.println(attribute.toString());
-        }
+//        List<Project> projects = projectService.selectList(new EntityWrapper<>());
+//        for(Project attribute : projects) {
+//          //  System.out.println(attribute.toString());
+//        }
 
         return "project/Project-Shouye";
     }
@@ -57,6 +59,98 @@ public class ProjectContorller {
 
         return "project/Project-Detail";
     }
+    @RequestMapping("/projectShouYeHtmls")
+    @ResponseBody
+    public Map<String, String> GetMegTest() {
+        Map<String, String>   result = new HashMap<>();
+        result.put("xia","1");
+        result.put("wei","1");
+
+        System.out.println("1---------------------");
+        return  result;
+    }
+
+
+    //用于获取两个推荐单之一的一个   的数据
+    //order by  实现  SELECT  *  FROM `project`  DESCRIBE sex LIMIT 2;
+    @RequestMapping("/projectShouYeTuijian")
+    @ResponseBody
+    public List<Project> GetMeg2() {
+
+        EntityWrapper<Project> wrapper = new EntityWrapper<Project>();
+        wrapper.orderBy("proj_starttime").last("limit 5");
+        List<Project> projects = projectService.selectList(wrapper);
+        for(Project p: projects){
+            System.out.println(p.toString());
+        }
+
+        return projects;
+    }
+    //用于获取两个推荐单之竞标最激烈的项目
+    //根据v-for 的。。来确认返回的类型
+    @RequestMapping("/projectShouYeHtml")
+    @ResponseBody
+    public  List<Project> GetMeg() {
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Integer> jingBiaomap = new HashMap<>();
+        Map<String, String>   lastresult = new HashMap<>();
+        Project projecttemp = new Project();
+        System.out.println("1---------------------++++++");
+        // 1.竞争最激烈的top10 项目---->> 竞标人数最多的项目------>>先来五个
+        // 2.首先查出所有的  项目！！！
+        List<Project> projects = projectService.selectList(new EntityWrapper<Project>());
+        List<Project> projectNeed = new ArrayList<Project>();
+
+        int count;
+        for(Project p: projects){
+       count =  biddingService.selectCount(new EntityWrapper<Bidding>().eq("bid_proj",p.getProjId()));
+        jingBiaomap.put( p.getProjId().toString(),count);
+
+        }
+        // -- 从网上找的对map 排序的方法
+        Map<String, Integer> result = jingBiaomap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+
+        System.out.println(jingBiaomap);
+        System.out.println(result);
+
+        int i = 0;
+
+        String href = null;
+        //两个数组进行存储
+        //
+        String phref[] = new String[5];
+        String pname[] = new String[5];
+        for(String value : result.keySet()){
+            if(i<5) {
+                projecttemp = projectService.selectById(value);
+
+            //对id 附上属性 --- href
+                href =  "projectDetail?id="+ value;
+
+//                phref[i] = href;
+//                pname[i] = projecttemp.getProjName();
+                projectNeed.add(projecttemp);
+//                System.out.println("id="+href);
+//                lastresult.put(href,projecttemp.getProjName());
+                i++;
+            }
+        }
+
+
+        map.put("hrefs",phref);
+        map.put("name",pname);
+        //竞标个数
+//        biddingService.selectCount(new EntityWrapper<Bidding>().eq("bid_proj",));
+
+
+
+        return  projectNeed;
+    }
+
     @RequestMapping("/projectDetailHtml")
     @ResponseBody
     public Map<String,Object> GetMessage(){
@@ -76,6 +170,9 @@ public class ProjectContorller {
         Project p = projectService.selectById(pid);  //查所有信息
         // 竞标人数
         int count=biddingService.selectCount(new EntityWrapper<Bidding>().eq("bid_proj",pid));
+        //
+       int restday = 10-projectService.projBidTimeNum(pid);
+
         //竞标成功时承接工作室的名字
         String studioname="";
         if(p.getProjStudio()!=null){
@@ -98,7 +195,7 @@ public class ProjectContorller {
             Endtime = p.getProjEndtime().toString().substring(0,10);
         else
             Endtime= null;
-        int restday = 1;
+//        int restday = 1;
 
         Boolean progressState1 = true;
         Boolean progressState2 = false;
@@ -129,8 +226,8 @@ public class ProjectContorller {
         String[] pros = new String[5];
         String[] moneys = new String[5];
         String[] tags = new String[5];
-//        int[] restdays = new int[5];
-        int[] restdays = {1,2,3,4,5};
+        int[] restdays = new int[5];
+//        int[] restdays = {1,2,3,4,5};
         int index = 0;
 
         //获取5条相关记录
@@ -142,7 +239,7 @@ public class ProjectContorller {
             pros[index] = project.getProjName();
             moneys[index] = project.getProjMoney();
             tags[index] = project.getProjTag();
-//            restdays[index] = 10-projectService.projBidTimeNum(project.getProjId());
+            restdays[index] = 10-projectService.projBidTimeNum(project.getProjId());
             index++;
             System.out.println(project.toString());
             if(index>=5)
@@ -170,6 +267,9 @@ public class ProjectContorller {
         map.put("endday",Endtime);
         map.put("studioname",studioname);
         map.put("promulgator",promName);
+        map.put("people",count);
+
+
 
         return map;
     }
