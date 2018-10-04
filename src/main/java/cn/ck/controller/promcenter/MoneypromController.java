9e8e34d8c3e13146c5a5ca19a535d2dea4476cdf
XN0624 <489281012@qq.com>
2018-10-04 23:08:00 +0800

    完成支付宝充值、提现功能（充值功能需在natapp运行下才能运行） 林晓能

--- src/main/java/cn/ck/controller/promcenter/MoneypromController.java
+++ src/main/java/cn/ck/controller/promcenter/MoneypromController.java
@@ -12,18 +12,23 @@ import cn.ck.utils.ResponseBo;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayFundTransToaccountTransferRequest;
 import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.alipay.api.response.AlipayFundTransToaccountTransferResponse;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
 import java.util.*;
 
 @Controller
@@ -47,12 +52,18 @@ public class MoneypromController {
     public ResponseBo priceprom(){
         Alluser user = (Alluser) SecurityUtils.getSubject().getPrincipal();
         Account account=accountService.selectOne(new EntityWrapper<Account>().eq("acc_foreid",user.getAllId()));
-        List<Funds> fundsList=fundsService.selectList(new EntityWrapper<Funds>().eq("fund_income",user.getAllId()));
+        Set<String> set = new HashSet<>();
+        set.add("fund_datetime");
+        List<Funds> fundsList=fundsService.selectList(new EntityWrapper<Funds>().eq("fund_income",user.getAllId()).orderDesc(set));
 
 
         return ResponseBo.ok().put("account",account).put("price",fundsList);
     }
 
+    /**
+     * 充值提现界面渲染、校验提现金额和支付密码的合法性
+     * @return
+     */
     @RequestMapping("/prompayin")
     @ResponseBody
     public ResponseBo prompayin(){
@@ -61,11 +72,13 @@ public class MoneypromController {
         Account account=accountService.selectOne(new EntityWrapper<Account>().eq("acc_foreid",user.getAllId()));
         String prom=promulgator.getPromName();
         double acc=account.getAccMoney();
-        return ResponseBo.ok().put("name",prom).put("money",acc);
+        String paypwd=promulgator.getPromPaypwd();
+        return ResponseBo.ok().put("name",prom).put("money",acc).put("pwd",paypwd);
     }
 
+
     /**
-     *
+     *充值操作
      * @Description: 前往支付宝第三方网关进行支付
      */
     @RequestMapping("/goAlipay")
@@ -80,13 +93,14 @@ public class MoneypromController {
         alipayRequest.setNotifyUrl(AlipayConfig.notify_url);
 
         //商户订单号，商户网站订单系统中唯一订单号，必填
-        String out_trade_no = String.valueOf(UUID.randomUUID());
+        String uuid=UUID.randomUUID().toString();
+        String out_trade_no = uuid.replace("-", "");
         //付款金额，必填
         String total_amount = paynumber;
         //订单名称，必填
-        String subject = "123456";
+        String subject = "充值";
         //商品描述，可空
-        String body = "用户订购商品个数：" + "12345678";
+        String body = "用户账户充值操作";
 
         // 该笔订单允许的最晚付款时间，逾期将关闭交易。取值范围：1m～15d。m-分钟，h-小时，d-天，1c-当天（1c-当天的情况下，无论交易何时创建，都在0点关闭）。 该参数数值不接受小数点， 如 1.5h，可转换为 90m。
         String timeout_express = "1c";
@@ -100,18 +114,17 @@ public class MoneypromController {
 
         //请求
         String result = alipayClient.pageExecute(alipayRequest).getBody();
-        System.out.println(alipayRequest.getNotifyUrl());
 
         return result;
     }
 
     /**
-     *
+     *充值操作
      * @Title: AlipayController.java
      * @Package com.sihai.controller
      * @Description: 支付宝同步通知页面
      */
-    @RequestMapping("/alipayReturnNotice")
+    @GetMapping("/alipayReturnNotice")
     public ModelAndView alipayReturnNotice(HttpServletRequest request, HttpServletRequest response) throws Exception {
         //获取支付宝GET过来反馈信息
         Map<String,String> params = new HashMap<String,String>();
@@ -142,13 +155,26 @@ public class MoneypromController {
             Promulgator promulgator=promulgatorService.selectID(user.getAllId());
             Account account=accountService.selectOne(new EntityWrapper<Account>().eq("acc_foreid",user.getAllId()));
             String promname=promulgator.getPromName();
-            double acc=account.getAccMoney()+Double.valueOf(total_amount);
+            double d1=Double.valueOf(total_amount);
+            BigDecimal bg = new BigDecimal(d1);
+            double acc = bg.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+            double accall=acc+account.getAccMoney();
 
-            mv.addObject("account", total_amount);
+            account.setAccMoney(accall);
+            accountService.updateAllColumnById(account);
+            Funds funds=new Funds();
+            funds.setFundId(out_trade_no);
+            funds.setFundDatetime(new Date());
+            funds.setFundType("充值");
+            funds.setFundIncome(user.getAllId());
+            funds.setFundOutlay("支付宝");
+            funds.setFundMoney(acc);
+            fundsService.insert(funds);
+            System.out.println(funds);
+
+            mv.addObject("account", acc);
             mv.addObject("name",promname);
-            mv.addObject("accountall",acc);
-            System.out.println(total_amount+"-----"+promname+"------"+acc);
-            System.out.println(trade_no);
+            mv.addObject("accountall",accall);
 
         }else {
 
@@ -157,14 +183,14 @@ public class MoneypromController {
     }
 
     /**
-     *
+     *充值操作
      * @Title: AlipayController.java
      * @Package com.sihai.controller
      * @Description: 支付宝异步 通知页面
      */
-    @RequestMapping("/alipayNotifyNotice")
+    @PostMapping("/alipayNotifyNotice")
     public String alipayNotifyNotice(HttpServletRequest request, HttpServletRequest response) throws Exception {
-        System.out.println("9999999");
+//        System.out.println("9999999");
         //获取支付宝POST过来反馈信息
         Map<String,String> params = new HashMap<String,String>();
         Map<String,String[]> requestParams = request.getParameterMap();
@@ -192,28 +218,77 @@ public class MoneypromController {
             //付款金额
             String total_amount = new String(request.getParameter("total_amount").getBytes("ISO-8859-1"),"UTF-8");
             if(trade_status.equals("TRADE_FINISHED")){
-                System.out.println("6666666");
+//                System.out.println("6666666");
             }else if (trade_status.equals("TRADE_SUCCESS")){
 
-                Alluser user = (Alluser) SecurityUtils.getSubject().getPrincipal();
-                Account account=accountService.selectOne(new EntityWrapper<Account>().eq("acc_foreid",user.getAllId()));
-                account.setAccMoney(account.getAccMoney()+Double.valueOf(total_amount));
-                accountService.updateAllColumnById(account);
-                Funds funds=new Funds();
-                funds.setFundId(out_trade_no);
-                funds.setFundDatetime(new Date());
-                funds.setFundType("充值");
-                funds.setFundIncome(user.getAllId());
-                funds.setFundOutlay("支付宝");
-                funds.setFundMoney(Double.valueOf(total_amount));
-                fundsService.insert(funds);
-                System.out.println(funds);
+
             }
-            System.out.println("111111");
+//            System.out.println("111111");
             return "success";
         }else {//验证失败
-            System.out.println("123456");
+//            System.out.println("123456");
             return "fail";
         }
     }
+
+    /**
+     * 支付宝提现操作
+     * @return
+     */
+    @RequestMapping("/alipayout")
+    public ModelAndView alipayout(HttpServletRequest req, HttpServletRequest res)throws Exception{
+        //商户转账唯一订单号
+        String uuid=UUID.randomUUID().toString();
+        String out_biz_no = uuid.replace("-", "");
+        //收款方账户类型
+        String payee_type="ALIPAY_LOGONID";
+        //收款方账户
+        Alluser user = (Alluser) SecurityUtils.getSubject().getPrincipal();
+        Promulgator promulgator=promulgatorService.selectID(user.getAllId());
+        String promname=promulgator.getPromName();
+        String payee_account=promulgator.getPromAbipay();
+        //转账金额
+        String amount=req.getParameter("payoutnumber");
+
+        ModelAndView mv = new ModelAndView("/promulgator/prom_payoutresult");
+
+        AlipayClient alipayClient = new DefaultAlipayClient(AlipayConfig.gatewayUrl,AlipayConfig.app_id,AlipayConfig.merchant_private_key,"json",AlipayConfig.charset,AlipayConfig.alipay_public_key,AlipayConfig.sign_type);
+        AlipayFundTransToaccountTransferRequest request = new AlipayFundTransToaccountTransferRequest();
+        request.setBizContent("{" +
+                "    \"out_biz_no\":\""+out_biz_no+"\"," +
+                "    \"payee_type\":\"ALIPAY_LOGONID\"," +
+                "    \"payee_account\":\""+payee_account+"\"," +
+                "    \"amount\":\""+amount+"\"," +
+                "  }");
+        AlipayFundTransToaccountTransferResponse response = alipayClient.execute(request);
+        if(response.isSuccess()){
+//            System.out.println(amount);
+            Account account=accountService.selectOne(new EntityWrapper<Account>().eq("acc_foreid",user.getAllId()));
+            double d1=Double.valueOf(amount);
+            BigDecimal bg = new BigDecimal(d1);
+            double acc = bg.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+            double accall=account.getAccMoney()-acc;
+            account.setAccMoney(accall);
+
+            accountService.updateAllColumnById(account);
+            Funds funds=new Funds();
+            funds.setFundId(out_biz_no);
+            funds.setFundDatetime(new Date());
+            funds.setFundType("提现");
+            funds.setFundIncome(user.getAllId());
+            funds.setFundOutlay("支付宝");
+            funds.setFundMoney(-acc);
+            fundsService.insert(funds);
+            System.out.println(funds);
+
+            mv.addObject("account", acc);
+            mv.addObject("name",promname);
+            mv.addObject("accountall",accall);
+        } else {
+            System.out.println("error");
+        }
+        return mv;
+    }
+
+
 }
--- src/main/resources/static/js/js_lxn/prom_pay.js
+++ src/main/resources/static/js/js_lxn/prom_pay.js
@@ -26,6 +26,36 @@ $(function() {
 		var length=value.length;
 		return this.optional(element)||(length==6);
 	},"请输入6位支付密码");
+    jQuery.validator.addMethod("paypwdequal",function (value,element) {
+    	var flag=1;
+    	$.ajax({
+			url:'/promcenter/prompayin',
+            async:false,
+            success:function (result) {
+				if(value==result.pwd){
+					flag=1;
+				}else{
+					flag=0;
+                }
+            }
+		});
+        return this.optional(element)||(flag==1);
+    },"请输入正确支付密码");
+    jQuery.validator.addMethod("payoutnumtrue",function (value,element) {
+        var flag=1;
+        $.ajax({
+            url:'/promcenter/prompayin',
+            async:false,
+            success:function (result) {
+                if(value > result.money){
+                    flag=0;
+                }else{
+                    flag=1;
+                }
+            }
+        });
+        return this.optional(element)||(flag==1&&value>0);
+    },"提现金额不能超过现有账户余额");
 })
 //以下为官方示例
 $().ready(function() {
@@ -36,20 +66,32 @@ $().ready(function() {
 					required: true,
 					number:true,
 				},
-				paypwd:{
-					required: true,
-					paypwdlength:true,
-				}
+                payoutnumber: {
+                    required: true,
+                    number:true,
+                    payoutnumtrue:true,
+                },
+                paypwd:{
+                    required: true,
+                    paypwdlength:true,
+                    paypwdequal:true,
+                },
 			},
 			messages: {
 				paynumber: {
 					required: icon + "充值金额不能为空，请重新输入",
 					number:icon + "请输入有效数字",
 				},
-				paypwd:{
-					required: icon + "支付密码不能为空，请重新输入",
-					paypwdlength:icon+"请输入6位支付密码"
-				}
+                payoutnumber: {
+                    required: icon + "充值金额不能为空，请重新输入",
+                    number:icon + "请输入有效数字",
+                    payoutnumtrue:icon + "提现金额不能超过现有账户余额",
+                },
+                paypwd:{
+                    required: icon + "支付密码不能为空，请重新输入",
+                    paypwdlength:icon+"请输入6位支付密码",
+                    paypwdequal:icon+"支付密码错误，请重新输入",
+                },
 			}
 	});
 });
