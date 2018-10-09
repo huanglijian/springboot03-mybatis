fe1bd7c62f753d7051f227dc00b26061d84935f7
Katevivi <914921032@qq.com>
2018-10-09 21:10:26 +0800

    hhm

---
+++ src/main/java/cn/ck/entity/bean/OriCol.java
@@ -0,0 +1,22 @@
+package cn.ck.entity.bean;
+
+import cn.ck.entity.Collectori;
+import cn.ck.entity.Original;
+
+
+/**
+ * Description:chuang
+ * Created by Ming on 2018/9/29 23:44
+ */
+public class OriCol extends Original {
+    private Collectori collectori;
+
+
+    public Collectori getCollectres() {
+        return collectori;
+    }
+
+    public void setCollectori(Collectori collectori) {
+        this.collectori = collectori;
+    }
+}
