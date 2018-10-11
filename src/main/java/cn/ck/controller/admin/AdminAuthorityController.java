5a6b86971c51698090a7b64da2879ad5fcee25a4
AchmGitHub <670509577@qq.com>
2018-10-11 16:53:22 +0800

    footable文件 陈文翰

--- src/main/java/cn/ck/controller/admin/AdminAuthorityController.java
+++ src/main/java/cn/ck/controller/admin/AdminAuthorityController.java
@@ -2,16 +2,21 @@ package cn.ck.controller.admin;
 
 import cn.ck.controller.AbstractController;
 import cn.ck.service.AdminService;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiresRoles("admin")
 @RequestMapping("admin")
 public class AdminAuthorityController extends AbstractController {
 
     @Autowired
     private AdminService adminService;
 
-
+    @RequestMapping("test")
+    public String test(){
+        return "admin/alluser";
+    }
 }
