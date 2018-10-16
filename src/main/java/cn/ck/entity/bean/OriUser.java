51267d8a4727356a251f8f8cb68e4cb27f770ae5
Katevivi <914921032@qq.com>
2018-10-16 16:43:40 +0800

    hhm

---
+++ src/main/java/cn/ck/entity/bean/OriUser.java
@@ -0,0 +1,20 @@
+package cn.ck.entity.bean;
+
+import cn.ck.entity.Original;
+import cn.ck.entity.Users;
+
+/**
+ * Description:chuang
+ * Created by Ming on 2018/10/9 17:25
+ */
+public class OriUser extends Original {
+    private Users users;
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+}
