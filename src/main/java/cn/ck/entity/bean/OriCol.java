df8f08a692699dd423e300d22558d3aed52f8aca
Katevivi <914921032@qq.com>
2018-10-16 16:36:45 +0800

    hhm

--- src/main/java/cn/ck/entity/bean/OriCol.java
+++ src/main/java/cn/ck/entity/bean/OriCol.java
@@ -3,10 +3,15 @@ package cn.ck.entity.bean;
 import cn.ck.entity.Collectori;
 import cn.ck.entity.Original;
 
+
+
+
 public class OriCol extends Original {
     private Collectori collectori;
 
+
     public Collectori getCollectori() {
+
         return collectori;
     }
 
